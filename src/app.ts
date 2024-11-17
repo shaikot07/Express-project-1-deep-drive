afd3b14c128faefdcf75deeeabcbfe92142a8e97
shaikot07 <saifulislamshaikat007@gmail.com>
2024-11-17 22:31:45 +0600

    clien app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,3 @@
-// // const express = require('express')
-// import express from 'express'
-// const app = express()
-// const port = 3000
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-// // app.listen(port, () => {
-// //   console.log(`Example app listening on port ${port}`)
-// // })
-
-// export default app
-
 
 import express, { NextFunction, Request, Response } from "express";
 import { join } from "path";
